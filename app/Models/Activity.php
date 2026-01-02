6a6eefb18d48edbf0219bd14138342c8fbf220c1
RIO <ryobabaraka197@gmail.com>
2026-01-02 12:26:47 +0300

    Create Activity model with relationships and fillable fields

---
+++ app/Models/Activity.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'activities';
+    
+    protected $fillable = [
+        'project_id',
+        'assigned_to',
+        'title',
+        'description',
+        'start_date',
+        'end_date',
+        'status',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+    
+    public function assignedTo()
+    {
+        return $this->belongsTo(Employee::class, 'assigned_to');
+    }
+}
